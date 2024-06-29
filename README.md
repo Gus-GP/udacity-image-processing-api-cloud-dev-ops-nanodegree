1850cfb615367b80d94caf599e0a85106b710899
Gustavo Grinsteins <gusgrinsteins@gmail.com>
2024-06-29 12:22:58 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Full Stack Apps on AWS Project
+
+You have been hired as a software engineer to develop an application that will help the FBI find missing people.  The application will upload images to the FBI cloud database hosted in AWS. This will allow the FBI to run facial recognition software on the images to detect a match. You will be developing a NodeJS server and deploying it on AWS Elastic Beanstalk. 
+You will build upon the application we've developed during the lessons in this course. You'll complete a REST API endpoint in a backend service that processes incoming image URLs.
+
+## Getting Started
+
+You can clone this repo to run the project locally, or navigate to the workspace in the Udacity course.
+
+## Project Instructions
+
+To complete this project, you will need to:
+
+* Set up node environment
+* Create a new endpoint in the server.js file
+* Deploying your system
+
+## Testing
+
+Successful URL responses should have a 200 code. Ensure that you include error codes for the scenario that someone uploads something other than an image and for other common errors.
+
+Example requests:
+
+- {HOST}:{PORT}/filteredimage?image_url=https://upload.wikimedia.org/wikipedia/commons/5/5c/L-alanine-3D-balls.png
+- {HOST}:{PORT}/filteredimage?image_url=https://upload.wikimedia.org/wikipedia/commons/3/32/Cingulata2.jpg
+- {HOST}:{PORT}/filteredimage?image_url=https://upload.wikimedia.org/wikipedia/en/d/dc/Dark_Matter_TV_series_logo.jpg
+
+## License
+
+[License](LICENSE.txt)
---
+++ server.js
@@ -0,0 +1,84 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles, isValidUrl} from './util/util.js';
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+    /**************************************************************************** */
+  //! END @TODO1
+  // Get tweet by id
+  app.get( "/filteredimage", async ( req, res ) => {
+    let { image_url } = req.query;
+
+    console.log(image_url)
+
+    if ( !image_url ) {
+      return res.status(400).send("Hello! please provide and define the image_url field :) example: /filteredimage?image_url=cool.png");
+    }
+
+    if( !isValidUrl(image_url) ) {
+      return res.status(400).send("Hello! looks like something is off with your image URL, could you double check and try again? Thanks :)");
+    }
+
+    try {
+
+      let out_image_file = null
+
+      await filterImageFromURL(image_url).then((out_file_path) => {
+        out_image_file = out_file_path
+        console.log("The image file %s was processed locally: %s", image_url, out_file_path);
+      }).catch(() => res.status(500).send("Hello! Something went wrong processing your image! Have a nice day!"));
+
+      return res.status(200).sendFile(out_image_file, async () => {
+
+        console.log("Filtered Image File sent as a response");
+
+      await deleteLocalFiles([out_image_file]).then(() => {
+        console.log("The local image file %s was deleted", out_image_file);
+      }).catch(() => res.status(500).send("Hello! Something went wrong deleting your image! Have a nice day!"));;
+
+      });
+
+    } 
+    
+    catch (error) {
+
+      let errorString = 'Oh no! something went wrong. Please try again... error = ' + error
+
+      return res.status(500).send(errorString);
+      
+    }
+  } );
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
